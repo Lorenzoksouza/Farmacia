5424de56558b2f96267fe64b10dd970a3ef5801e
lorenzo <Lorenzoksouza@hotmail.com>
2019-05-14 18:14:30 -0300

    Start

---
+++ Farmacia/src/model/vo/Remedio.java
@@ -0,0 +1,65 @@
+package model.vo;
+
+public class Remedio {
+	
+	private int idRemedio;
+	private String nome;
+	private double preco;
+	private String dosagem;
+	private String tipo;
+	private boolean generico;
+	
+	public int getIdRemedio() {
+		return idRemedio;
+	}
+	public void setIdRemedio(int idRemedio) {
+		this.idRemedio = idRemedio;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getDosagem() {
+		return dosagem;
+	}
+	public void setDosagem(String dosagem) {
+		this.dosagem = dosagem;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public boolean isGenerico() {
+		return generico;
+	}
+	public void setGenerico(boolean generico) {
+		this.generico = generico;
+	}
+	
+	public Remedio(int idRemedio, String nome, double preco, String dosagem, String tipo, boolean generico) {
+		super();
+		this.idRemedio = idRemedio;
+		this.nome = nome;
+		this.preco = preco;
+		this.dosagem = dosagem;
+		this.tipo = tipo;
+		this.generico = generico;
+	}
+	
+	public Remedio() {
+		super();
+	}
+	
+	
+	
+}
