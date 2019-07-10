bf5119f20a7f2c369125c41c24c1caceee0e7645
MatheusAHM <matheusassis1998@hotmail.com>
2019-07-10 09:25:03 -0300

    linkreferencia

--- Farmacia/src/view/TelaSobre.java
+++ Farmacia/src/view/TelaSobre.java
@@ -1,13 +1,20 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.border.LineBorder;
@@ -41,7 +48,7 @@ public class TelaSobre extends JInternalFrame {
 		setBackground(Color.WHITE);
 		setFrameIcon(new ImageIcon(TelaSobre.class.getResource("/icons/network.png")));
 		setBorder(new LineBorder(Color.LIGHT_GRAY, 3));
-		setBounds(500, 100, 340, 225);
+		setBounds(500, 100, 340, 250);
 		getContentPane().setLayout(new MigLayout("", "[grow,fill]", "[][][][][][][]"));
 
 		JLabel lblDesenvolvedores = new JLabel("Desenvolvedores:");
@@ -64,23 +71,44 @@ public class TelaSobre extends JInternalFrame {
 		lblVitor.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblVitor, "cell 0 3,alignx center");
 
-		JLabel lblContatoMatheusmartinsalunoscsenacbr = new JLabel("Contato:");
-		lblContatoMatheusmartinsalunoscsenacbr.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblContatoMatheusmartinsalunoscsenacbr.setHorizontalAlignment(SwingConstants.LEFT);
-		getContentPane().add(lblContatoMatheusmartinsalunoscsenacbr, "flowx,cell 0 4,growx");
+		JLabel lblContato = new JLabel("Ajuda:");
+		lblContato.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContato.setHorizontalAlignment(SwingConstants.LEFT);
+		getContentPane().add(lblContato, "flowx,cell 0 5,alignx left");
 
-		JTextArea txtrMatheusmartinsalunoscsenacbr = new JTextArea();
-		txtrMatheusmartinsalunoscsenacbr.setEditable(false);
-		txtrMatheusmartinsalunoscsenacbr.setFont(new Font("Tahoma", Font.BOLD, 13));
-		txtrMatheusmartinsalunoscsenacbr.setText("matheus.martins@aluno.sc.senac.br");
-		getContentPane().add(txtrMatheusmartinsalunoscsenacbr, "cell 0 4,growx");
-
-		JLabel lblV = new JLabel("V.1.0");
-		getContentPane().add(lblV, "flowx,cell 0 5");
+		JLabel lblV = new JLabel("V.2.0");
+		getContentPane().add(lblV, "flowx,cell 0 6");
 
 		JLabel label = new JLabel("\u00A9 2019");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
-		getContentPane().add(label, "cell 0 5");
+		getContentPane().add(label, "cell 0 6");
+
+		JButton txtrMatheusmartinsalunoscsenacbr = new JButton();
+		txtrMatheusmartinsalunoscsenacbr.setBackground(Color.WHITE);
+		txtrMatheusmartinsalunoscsenacbr.setOpaque(true);
+		txtrMatheusmartinsalunoscsenacbr.setPreferredSize(new Dimension(80, 30));
+		txtrMatheusmartinsalunoscsenacbr.setBorder(new LineBorder(Color.gray, 0, true));
+		txtrMatheusmartinsalunoscsenacbr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					URI uri = new URI("https://support.apple.com/ar-ae");
+					try {
+						Desktop.getDesktop().browse(uri);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		txtrMatheusmartinsalunoscsenacbr.setFont(new Font("Tahoma", Font.BOLD, 13));
+		txtrMatheusmartinsalunoscsenacbr.setText("support.uol.net/ayudame");
+
+		getContentPane().add(txtrMatheusmartinsalunoscsenacbr, "cell 0 5,growx");
 		String name = UIManager.getSystemLookAndFeelClassName();
 
 	}
